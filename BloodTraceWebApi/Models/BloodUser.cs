986f9b496b2cc034888a6f77a70e3fbd0049fcf8
desmondmm <mutsakaradesmond@gmail.com>
2018-09-12 01:32:52 +0200

    Added a blooduser model

---
+++ BloodTraceWebApi/Models/BloodUser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace BloodTraceWebApi.Models
+{
+    public class BloodUser
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        [RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$",ErrorMessage = "Please enter a valid email address")]
+        public string Email { get; set; }
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Please enter a valid phone number")]
+        public string Phone { get; set; }
+        public string Country { get; set; }
+        public string BloodGroup { get; set; }
+        public string ImagePath { get; set; }
+        public int Date { get; set; }
+
+        [NotMapped]
+        public byte ImageArray { get; set; }
+    }
+}
